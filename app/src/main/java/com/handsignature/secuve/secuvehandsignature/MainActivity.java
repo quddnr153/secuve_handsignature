60b68b281faf3c8f33a8a57f73f94423bf9a0517
Byungwook Lee <quddnr153@gmail.com>
2016-08-12 12:45:00 +0900

    Add register activity for writting down name, and change the main activity

--- app/src/main/java/com/handsignature/secuve/secuvehandsignature/MainActivity.java
+++ app/src/main/java/com/handsignature/secuve/secuvehandsignature/MainActivity.java
@@ -6,6 +6,9 @@ package com.handsignature.secuve.secuvehandsignature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button launch = (Button)findViewById(R.id.registerButton);
+        launch.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class); // 두번째 액티비티를 실행하기 위한 인텐트
+                startActivity(intent); // 두번째 액티비티를 실행합니다.
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/handsignature/secuve/secuvehandsignature/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.handsignature.secuve.secuvehandsignature;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by quddn on 2016-08-12.
+ */
+public class RegisterActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registername);
+    }
+}
